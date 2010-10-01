f59130dc4adf59aadcd8f10baa4a378c2dbf3eed
divmod <joydivasian@gmail.com>
2010-10-01 12:47:15 -0500

    All files from /microblog folder

---
+++ drop_blog.sql
@@ -0,0 +1,11 @@
+--
+-- This elliminates the schema from the database
+--
+DROP TABLE blog_messages;
+DROP TABLE blog_permissions;
+DROP TABLE blog_actions;
+DROP TABLE blog_users;
+DROP sequence blog_message_id;
+
+quit;
+
